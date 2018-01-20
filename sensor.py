6d866c01fbd56b96a502fea21fdc5177220bdb9d
Scott Umsted <scottumsted@gmail.com>
2018-01-20 07:29:07 -0600

    add try block

--- sensor.py
+++ sensor.py
@@ -16,26 +16,29 @@ def start():
     with open('ultrasonic_sensors.txt', 'r+') as f:
         with contextlib.closing(mmap.mmap(f.fileno(), 0)) as m:
             while True:
-                # replace random with a call to read each sensor
-                sensors = {'front': randint(20, 750),
-                           'left': randint(20, 750),
-                           'right': randint(20, 750)}
-                # simulate delay of calling three sensors
-                time.sleep(.3)
+                try:
+                    # replace random with a call to read each sensor
+                    sensors = {'front': randint(20, 750),
+                               'left': randint(20, 750),
+                               'right': randint(20, 750)}
+                    # simulate delay of calling three sensors
+                    time.sleep(.3)
 
-                sensors_json = str.encode(json.dumps(sensors))  # serialize data into a bytes
+                    sensors_json = str.encode(json.dumps(sensors))  # serialize data into a bytes
 
-                m.seek(0)  # rewind memory map and clear buffer
-                m.write(str.encode(CLEAR))
-                m.flush()
+                    m.seek(0)  # rewind memory map and clear buffer
+                    m.write(str.encode(CLEAR))
+                    m.flush()
 
-                m.seek(0)  # rewind memory map and write data
-                m.write(sensors_json)
-                m.flush()
+                    m.seek(0)  # rewind memory map and write data
+                    m.write(sensors_json)
+                    m.flush()
 
-                m.seek(0)  # rewind and test read
-                print('line :%s' % m.readline())
-
+                    m.seek(0)  # rewind and test read
+                    print('line :%s' % m.readline())
+                except Exception as e:
+                    print("Exception: %s" % str(e))
+                    # don't let this fail
                 time.sleep(.1)
 
 
